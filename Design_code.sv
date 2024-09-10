4b0780bfe20ee74ef437d6e84f42b34bace7a304
RupamSen <147425684+RupamSen@users.noreply.github.com>
2024-09-10 13:14:38 +0530

    Update Design_code.sv

--- Design_code.sv
+++ Design_code.sv
@@ -1,57 +1,66 @@
-// Design Code
-`timescale 1s/1ns
+// Design Module: Smart Traffic Light Controller
 
-module traffic_signal (input rst, clk, traffic, output reg [1:0]signal);
+`timescale 1s/1ms
+
+module traffic_signal ( input logic rstn, 
+			                  input logic clk, 
+			                  input logic traffic,
+			                  output logic [1:0] signal
+			                );
   
-// Declare the Parameters and Internal signals
-  parameter RED = 2'b00, YELLOW = 2'b01, GREEN = 2'b10;
-  reg [1:0] pr_state, nxt_state;
+	
+  	typedef enum {RED = 0, YELLOW = 1, GREEN = 2} state_type;
+
+	  state_type pr_state, nxt_state;
   
-// Workflow of FSM
-  always @(posedge clk) begin   
-            if (rst) pr_state <= RED;
-            else 	 pr_state <= nxt_state;    
-  end
+	  logic [7:0] count;
+	
+
+
+  	always @(negedge rstn or negedge clk) begin   
+            if (~rstn)	begin pr_state <= RED; end
+            else 	begin pr_state <= nxt_state; end   
+  	end
   
-  
-  //Function of every State
-  always @(pr_state) begin
-    
-    case(pr_state)      
-      RED:			if (traffic) begin
-                  	signal = RED;
-                    repeat(4) @(posedge clk);
-          				  nxt_state = YELLOW;
-        			end
-        			else begin
-                    signal = RED;
-          				  repeat(6) @(posedge clk);
-          				  nxt_state = YELLOW;
-        			end
+
+    //Traffic Signal Control  
+  	always @(negedge rstn or posedge clk) begin   
+		        if (~rstn)	      begin count <= 'd0; end
+		        else 
+		         begin
+    			      case(pr_state)      
+      				            RED:	begin
+						                    signal <= RED; 
+				
+						                    if (traffic) begin
+							                                if (count < 'd5)	begin nxt_state <= RED;     count <= count + 1; end
+							                                else			        begin nxt_state <= YELLOW;  count <= 0;        end
+						                    end else begin
+							                                if (count < 'd2)	begin nxt_state <= RED;     count <= count + 1; end
+							                                else			        begin nxt_state <= YELLOW;  count <= 0;         end
+						                          end
+                  			        end
      		
-      YELLOW:		begin
-                  	signal = YELLOW;
-          			  	@(posedge clk);
-          				  nxt_state = GREEN;
-        			end
-        			
-      			
-      GREEN:		if (traffic) begin
-                    signal = GREEN;
-          				  repeat(9) @(posedge clk);
-          				  nxt_state = RED;
-        			end
-        			else begin
-                  	signal = GREEN;
-          				  repeat(6) @(posedge clk);
-          				  nxt_state = RED;
-        			end
+      			           YELLOW: 	begin
+                  				        signal <= YELLOW;
+          				  	            nxt_state <= GREEN;
+        				                end
+        			      			
+      			            GREEN:	begin
+						                    signal <= GREEN; 
+				
+						                    if (traffic) begin
+							                                if (count < 'd5)	begin nxt_state <= GREEN; count <= count + 1; end
+							                                else			        begin nxt_state <= RED;   count <= 0;         end
+						                    end else begin
+							                                if (count < 'd2)	begin nxt_state <= GREEN; count <= count + 1; end
+							                                else			        begin nxt_state <= RED;   count <= 0;         end
+						                        end
+                  			        end
 
-      default:	  nxt_state = RED;
-    endcase
-    
-  end
+      			        default:	begin  nxt_state = RED;  end
+    			  endcase    
+		    end
+  	end
       
 endmodule
-      
-          
